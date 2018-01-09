93a36ddce5f79c7f7ef0721c603d053ae6260f43
oscar <oscar_vanegas1@hotmail.com>
2018-01-09 01:47:27 -0500

    21

---
+++ Educacion/javascript/js/intro.js
@@ -0,0 +1,3 @@
+
+
+alert ("oh yeah1!");
---
+++ Educacion/javascript/js/variables.js
@@ -0,0 +1,33 @@
+
+
+var amigos = ["jaimer","julian","juan carlos","pablo", "juan"];
+
+//document.write(amigos[3]);
+
+
+//document.write("tienes" + " " + amigos.length + " " + "amigos");
+
+
+//amigos.pop();
+
+
+
+
+//var amigos2 = ["jaider","felipe"];
+//var amigos3 = amigos.concat(amigos2);
+//document.write(amigos3);
+
+//document.write(amigos.join(", "));  
+
+//amigos.length
+
+
+
+//nombre = prompt("ingresa tu nombre");
+
+//alert(nombre);
+
+//document.write(nombre);
+
+var ordenados = amigos.sort();
+document.write( ordenados );
